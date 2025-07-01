290347473a752d89c0a386dfb78fdbd687e77adc
Flex Coder <jaychonkar617@gmail.com>
2025-07-01 16:52:41 +0530

    index.py

--- index.py
+++ index.py
@@ -24,12 +24,15 @@ def watermark():
         draw = ImageDraw.Draw(watermark_layer)
 
         font = ImageFont.load_default()
-        text_width, text_height = draw.textsize(text, font)
+
+        bbox = draw.textbbox((0, 0), text, font=font)
+        text_width = bbox[2] - bbox[0]
+        text_height = bbox[3] - bbox[1]
 
         x = (width - text_width) // 2
         y = (height - text_height) // 2
 
-        draw.text((x, y), text, font=font, fill=(255, 255, 255, 80))  # white, light opacity
+        draw.text((x, y), text, font=font, fill=(255, 255, 255, 80))
 
         watermarked = Image.alpha_composite(base, watermark_layer)
         output = BytesIO()
